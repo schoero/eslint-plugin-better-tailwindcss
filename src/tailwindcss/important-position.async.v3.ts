20f2ff016b561dd6e8bb63f4bd2aa98935c08859
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-07-13 09:02:57 +0200

    feat: new rule `enforce-consistent-important-position` (#167)
    * feat: new rule `enforce-consistent-important-position`
    
    * docs: add `enforce-consistent-important-position`
    
    * refactor: add position
    
    * feat: support tailwindcss 3
    
    * chore: clean up types
    
    * docs: remove examples

---
+++ src/tailwindcss/important-position.async.v3.ts
@@ -0,0 +1,44 @@
+import { getClassVariants } from "./class-variants.async.v3.js";
+import { getPrefix } from "./prefix.async.v3.js";
+
+import type { GetImportantPositionResponse, Position } from "./important-position.js";
+
+
+export function getImportantPosition(context: any, classes: string[], position: Position): GetImportantPositionResponse {
+  const variants = getClassVariants(context, classes);
+  const prefix = getPrefix(context);
+  const separator = context.tailwindConfig.separator ?? ":";
+
+  return classes.reduce<GetImportantPositionResponse>((acc, className) => {
+    const classVariants = variants.find(([name]) => name === className)?.[1] ?? [];
+
+    let base = className
+      .replace(classVariants.join(separator), "")
+      .replace(prefix, "")
+      .replace(/^:/, "");
+
+    const isImportantAtStart = base.startsWith("!");
+    base = base.replace(/^!/, "");
+
+    const isImportantAtEnd = base.endsWith("!");
+    base = base.replace(/!$/, "");
+
+    if(
+      !isImportantAtStart && !isImportantAtEnd ||
+      position === "legacy" && isImportantAtStart ||
+      position === "recommended" && isImportantAtEnd
+    ){
+      return acc;
+    }
+
+    if(isImportantAtStart && position === "recommended"){
+      acc[className] = [[...classVariants, prefix, base].filter(Boolean).join(separator), "!"].join("");
+    }
+
+    if(isImportantAtEnd && position === "legacy"){
+      acc[className] = [[...classVariants, "!"].filter(Boolean).join(separator), prefix, base].join("");
+    }
+
+    return acc;
+  }, {});
+}

0e476eafad5b93baf4b03e8e5e239c0e33c06ad4
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2025-10-20 21:17:36 +0200

    fix: fallback for unsupported tailwindcss versions

--- src/tailwindcss/canonical-classes.async.v4.ts
+++ src/tailwindcss/canonical-classes.async.v4.ts
@@ -2,5 +2,5 @@ import type { CanonicalClass } from "./canonical-classes.js";
 
 
 export function getCanonicalClasses(tailwindContext: any, classes: string[]): CanonicalClass[] {
-  return tailwindContext.canonicalizeCandidates(classes);
+  return tailwindContext?.canonicalizeCandidates?.(classes) ?? classes;
 }

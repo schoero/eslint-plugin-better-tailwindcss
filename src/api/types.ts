816b7ad36bfd53b4eeac58b91868590bdc2df526
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2026-01-25 19:18:27 +0100

    fix: exported types

--- src/api/types.ts
+++ src/api/types.ts
@@ -1,1 +1,8 @@
-export * from "better-tailwindcss:types/rule.js";
+export type {
+  ObjectKeyMatcher,
+  ObjectValueMatcher,
+  Regex,
+  StringMatcher
+} from "better-tailwindcss:types/rule.js";
+
+export { MatcherType } from "better-tailwindcss:types/rule.js";

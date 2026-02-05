1a01ffd1261fd26008792d7df7e3e3753430e535
Kim Haeram <ramram1048@gmail.com>
2026-02-05 20:49:14 +0900

    feat: Add `curried` callee support

--- src/options/schemas/callees.ts
+++ src/options/schemas/callees.ts
@@ -1,5 +1,6 @@
 import {
   array,
+  boolean,
   description,
   optional,
   pipe,
@@ -19,34 +20,51 @@ import {
 import type { InferOutput } from "valibot";
 
 
+const CALLEE_NAME_STRING_SCHEMA = pipe(
+  string(),
+  description("Callee name for which children get linted.")
+);
+
+const CALLEE_NAME_OBJECT_SCHEMA = strictObject({
+  curried: optional(
+    pipe(
+      boolean(),
+      description("Whether to match curried function calls recursively.")
+    )
+  ),
+  name: CALLEE_NAME_STRING_SCHEMA
+});
+
+const CALLEE_NAME_SCHEMA = union([
+  CALLEE_NAME_STRING_SCHEMA,
+  CALLEE_NAME_OBJECT_SCHEMA
+]);
+
+export type CalleeNameObject = InferOutput<typeof CALLEE_NAME_OBJECT_SCHEMA>;
+export type CalleeName = InferOutput<typeof CALLEE_NAME_SCHEMA>;
+
+const CALLEE_MATCHER_LIST_SCHEMA = pipe(
+  array(
+    union([
+      STRING_MATCHER_SCHEMA,
+      OBJECT_KEY_MATCHER_SCHEMA,
+      OBJECT_VALUE_MATCHER_SCHEMA
+    ])
+  ),
+  description("List of matchers that will be applied.")
+);
+
+export type CalleeMatcherList = InferOutput<typeof CALLEE_MATCHER_LIST_SCHEMA>;
+
 const CALLEE_MATCHER_SCHEMA = pipe(
   tuple([
-    pipe(
-      string(),
-      description("Callee name for which children get linted if matched.")
-    ),
-    pipe(
-      array(
-        union([
-          STRING_MATCHER_SCHEMA,
-          OBJECT_KEY_MATCHER_SCHEMA,
-          OBJECT_VALUE_MATCHER_SCHEMA
-        ])
-      ),
-      description("List of matchers that will be applied.")
-    )
+    CALLEE_NAME_SCHEMA,
+    CALLEE_MATCHER_LIST_SCHEMA
   ]),
   description("List of matchers that will automatically be matched.")
 );
 
 export type CalleeMatchers = InferOutput<typeof CALLEE_MATCHER_SCHEMA>;
-
-const CALLEE_NAME_SCHEMA = pipe(
-  string(),
-  description("Callee name for which children get linted.")
-);
-
-export type CalleeName = InferOutput<typeof CALLEE_NAME_SCHEMA>;
 
 export const CALLEES_SCHEMA = pipe(
   array(
@@ -65,3 +83,5 @@ export const CALLEES_OPTION_SCHEMA = strictObject({
 });
 
 export type CalleesOptions = InferOutput<typeof CALLEES_OPTION_SCHEMA>;
+
+export type NormalizedCallee = [CalleeNameObject, CalleeMatcherList?];

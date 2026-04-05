aeefd96aedcc94ffc31630a9e84bdb63c387c5bf
Roger Schönbächler <42278642+schoero@users.noreply.github.com>
2026-04-05 09:49:29 +0200

    perf: cache project root

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,5 +1,6 @@
 import { dirname, resolve } from "node:path";
 
+import { withCache } from "better-tailwindcss:utils/cache.js";
 import { findPathRecursive } from "better-tailwindcss:utils/fs.js";
 
 import type { Rule } from "eslint";
@@ -7,7 +8,7 @@ import type { Rule } from "eslint";
 import type { CommonOptions } from "better-tailwindcss:options/descriptions.js";
 
 
-export function findProjectRoot(ctx: Rule.RuleContext, options: CommonOptions) {
+export const findProjectRoot = (ctx: Rule.RuleContext, options: CommonOptions) => withCache("project-root", options.entryPoint ?? options.tailwindConfig ?? options.tsconfig, () => {
   const { entryPoint, tailwindConfig, tsconfig } = options;
 
   const rootItems = [
@@ -26,4 +27,4 @@ export function findProjectRoot(ctx: Rule.RuleContext, options: CommonOptions) {
       return dirname(root);
     }
   }
-}
+});
